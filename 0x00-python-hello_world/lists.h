3b8a8e267ffecf46ab03c0f87ca7e6d0dce76515
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-05 12:47:11 -0700

    Update lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,23 +1,27 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ * struct listint_s - function for the singly linked lists.
+ * @z: integer.
+ * @next: points to the next node.
  *
- * Description: singly linked list node structure
+ * Description: singly linked list node structs.
  * 
  */
 typedef struct listint_s
 {
-    int n;
+    int z;
     struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint(listint_t **head, const int z);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
