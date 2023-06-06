0d7ab9ded3a2f0d50470a6e861aa427e7d6b48f6
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-06 14:15:55 -0700

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord("a"), ord("z") + 1):
-    if chr(i) == "q" or chr(i) == "e":
+for k in range(ord("a"), ord("z") + 1):
+    if chr(k) == "q" or chr(k) == "e":
         continue
     else:
-        print("{:s}".format(chr(i)), end="")
+        print("{:s}".format(chr(k)), end="")
