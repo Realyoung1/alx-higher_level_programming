8c6159808ba38a0adb1676de97ffaee77c05a68f
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-06 15:01:21 -0700

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-i = 122
-while i >= 97:
+k = 122
+while k >= 97:
     move = 0
-    if i % 2 != 0:
-        i = i - 32
+    if k % 2 != 0:
+        k = k - 32
         move = 1
-    print("{:s}".format(chr(i)), end="")
+    print("{:s}".format(chr(k)), end="")
     if move == 1:
-        i = i + 32
-    i = i - 1
+        k = k + 32
+    k = k - 1
