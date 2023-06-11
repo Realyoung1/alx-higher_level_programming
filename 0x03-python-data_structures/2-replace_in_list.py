735a14a95bf3d9c81933bae5727ef5013fcc33bc
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-11 06:41:48 -0700

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
-def replace_in_list(my_list, idx, element):
+def replace_in_list(my_list, vdx, element):
     """
-    replace an elment from a list at index idx with elem
+    replaces an elment from a list at index vdx with element
     Args:
-        my_list - list to search
-        idx - the position to access
+        my_list - function list to search.
+        vdx - function the position to access.
         element - new elem to swap with
     Return:
         my_list
     """
 
-    if idx < 0 or idx >= len(my_list):
+    if vdx < 0 or vdx >= len(my_list):
         return my_list
-    my_list[idx] = element
+    my_list[vdx] = element
     return my_list
