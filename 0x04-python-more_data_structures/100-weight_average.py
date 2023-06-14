6197fd3ab3cc2877a7e95a6d39670f71d6d6a074
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-14 16:25:19 -0700

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,10 +2,10 @@
 
 def weight_average(my_list=[]):
     """
-    a function that returns the weighted average of all integers tuple
+    a func that returns the weighted averages of all integers tuples.
     """
     if my_list is None or my_list == []:
         return 0
-    i_xy = sum(map(lambda a: a[0] * a[1], my_list))
-    i_x = sum(map(lambda a: a[1], my_list))
-    return i_xy / i_x
+    k_xy = sum(map(lambda a: a[0] * a[1], my_list))
+    k_x = sum(map(lambda a: a[1], my_list))
+    return k_xy / k_x
