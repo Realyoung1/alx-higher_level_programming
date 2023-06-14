ab0a226929b6c05d2306b094da8136a661e5ec5f
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-14 15:41:21 -0700

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
+
 def search_replace(my_list, search, replace):
     """
     search and replace an element in a list
     Args:
-        my_list - The list to search
-        search - element to replace
-        replace - subtitute for search
+        my_list - The function for the list to search.
+        search - the function of the elments to replaced.
+        replace - subtitute for searches.
     """
     if my_list is None:
         return None
