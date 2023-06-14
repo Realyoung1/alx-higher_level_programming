7855c615ed934d145784d29087713c994eb01bd9
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-14 15:48:49 -0700

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-def only_diff_elements(set_1, set_2):
+def only_diff_elements(set_6, set_9):
     """
-    find the symetric difference between 2 sets
+    find the symetrics diff bet 2 sets
     Args:
-        set_1 - first set
-        set_2 - second set
+        set_6 - first set
+        set_9 - second set
     """
-    return set_1.symmetric_difference(set_2)
+    return set_6.symmetric_difference(set_9)
