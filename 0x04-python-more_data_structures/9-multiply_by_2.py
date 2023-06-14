6d9b822c864fde862c5fc55af89a01649198fc0c
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-14 16:06:02 -0700

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-def multiply_by_2(a_dict):
+
+def multiply_by_2(m_dict):
     """
-    a function that returns a new dictionary with all values multiplied by 2
+    a func that returns a new dict with all values multiplied by 2
     """
-    if a_dict is None:
+    if m_dict is None:
         return None
     else:
-        return {key: a_dict[key] * 2 for key in a_dict}
+        return {key: m_dict[key] * 2 for key in m_dict}
