e8f70cb35f87fc83b6505689977ca85fc047c5c7
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-14 15:46:01 -0700

    Update 3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
+
 def common_elements(set_1, set_2):
     """
-    find the common elements between 2 sets
+    the functions that finds the common elements between 2 sets.
     Args:
-        set_1 - first set
-        set_2 - second set
+        set_6 - first set
+        set_9 - second set
     """
-    return set_1.intersection(set_2)
+    return set_6.intersection(set_9)
