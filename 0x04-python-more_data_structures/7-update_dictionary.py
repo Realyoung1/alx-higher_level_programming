431f213775fe9c14f56de03c0f07c375d8d2f6aa
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-14 15:59:22 -0700

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-def update_dictionary(a_dict, key, value):
+
+def update_dictionary(m_dict, key, value):
     """
     add or replace a new key value in dict
     """
-    if a_dict is None:
+    if m_dict is None:
         return None
-    a_dict[key] = value
-    return a_dict
+    m_dict[key] = value
+    return m_dict
