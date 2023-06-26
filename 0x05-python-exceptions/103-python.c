0c333243a6c0b9773cf3e51e82435312b52b3283
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-26 15:45:16 -0700

    Update 103-python.c

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,5 +1,16 @@
 #include <Python.h>
+
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
 #include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
 
 /**
  * print_python_float - gives data of the PyFloatObject
