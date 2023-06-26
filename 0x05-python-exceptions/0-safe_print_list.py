b540b53a1e7058da626cd52c2c4a57d9685a9600
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-26 15:26:48 -0700

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@
 def safe_print_list(my_list=[], x=0):
     a = 0
     try:
-        for i in range(x):
-            print(my_list[i], end='')
+        for k in range(x):
+            print(my_list[k], end='')
             a += 1
     except IndexError:
         None
