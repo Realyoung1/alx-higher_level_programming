a0762e5c874c4e355a4f491e25d0c53fbac10860
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-27 21:43:22 +0100

    Update 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Squares
+        Args:
+            size (int): These size of the new squares
+        """
+        self.__size = size
