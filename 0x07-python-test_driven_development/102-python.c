d768b82c95e55844397941bc9487cee4b4b7db82
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-07-02 11:22:04 -0700

    Update 102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,8 +1,21 @@
 #include "Python.h"
 
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+
+
 /**
- * print_python_string - Prints information about Python strings.
- * @p: A PyObject string object.
+ * print_python_string - func to Prints information about Python stringss.
+ * @p: A PyObject string objectss.
  */
 void print_python_string(PyObject *p)
 {
