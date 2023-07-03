d2def05fd9c307f63ea3da45889a4764c117c129
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-07-03 07:00:01 -0700

    Update 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -31,7 +31,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the height of the Rectangle."""
+        """Get/set the height of the rect-angle more classes."""
         return self.__height
 
     @height.setter
