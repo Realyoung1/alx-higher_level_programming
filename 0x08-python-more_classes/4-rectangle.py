0f2b29877c134a08ed745399ffe7c700339268f1
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-07-03 07:10:29 -0700

    Update 4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -59,9 +59,9 @@ class Rectangle:
             return ("")
 
         rect = []
-        for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
-            if i != self.__height - 1:
+        for k in range(self.__height):
+            [rect.append('#') for d in range(self.__width)]
+            if k != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
 
