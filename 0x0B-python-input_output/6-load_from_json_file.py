1f2c9fa6a8a2d9d0854b03ad0fa25429231900af
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-07-11 04:20:12 -0700

    Update 6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Defining a JSON file-reading funct."""
+import json
+
+
+def load_from_json_file(filename):
+    """Create a Python object from a JSON file."""
+    with open(filename) as f:
+        return json.load(f)
