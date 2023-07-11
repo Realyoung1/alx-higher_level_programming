fd655339970a902ced2258de9812ff5f219560a3
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-07-11 04:15:35 -0700

    Update 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-''' functioning that writes an Object to a text file, using a JSON representations
+''' functioning that writes an Object to a text file, using a JSON representation
 '''
 import json
 
