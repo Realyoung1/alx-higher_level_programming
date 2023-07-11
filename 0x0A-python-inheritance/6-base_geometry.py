a7e3ce8fc4f77ee592cc7b8eb764e5b673e58740
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-07-11 05:15:21 +0100

    Create 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defining the basegeo metry class BaseGeo metry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
