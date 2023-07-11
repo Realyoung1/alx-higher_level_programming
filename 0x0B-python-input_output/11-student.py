dbfcaa598f5350130540c27165d61621d3a2b75d
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-07-11 04:05:03 -0700

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,21 +3,21 @@
 
 
 class Student:
-    """Represent a student."""
+    """Representing the student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+        """Initializing the new Student.
         Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+            first_name (str): The firsts name of the student.
+            last_name (str): The lasts name of the student.
+            age (int): The agess of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
+        """Get a dictionary representations of the Students.
         If attrs is a list of strings, represents only those attributes
         included in the list.
         Args:
