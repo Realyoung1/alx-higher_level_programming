2bdd74ec683101414582e30787fd0052d9c5e984
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-07-17 06:54:32 -0700

    Update test_base.py

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,64 +1,139 @@
 #!/usr/bin/python3
-"""Defined Rectangle Classes.
-"""
-
+import unittest
+from models.square import Square
+import sys
+from io import StringIO
+import pep8
+from models.base import Base
+import json
 from models.rectangle import Rectangle
-
-
-class Square(Rectangle):
-    """Moduled Representation of Squares.
+import os
+"""
+This modules contains all unittest for Base classes.
 """
 
-    def __init__(self, size, x=0, y=0, id=None):
-        """Initializing the Squares
-        """
-        super().__init__(size, size, x, y, id)
 
-    @property
-    def size(self):
-        """module Square size getter
+class TestBase(unittest.TestCase):
+    """
+    Classes of functions to run tests.
+    """
+    def setUp(self):
         """
-        return self.width
+        functions to redirects stdout.
+        """
+        sys.stdout = StringIO()
 
-    @size.setter
-    def size(self, value):
-        """module Square size setter
+    def tearDown(self):
         """
-        self.width = value
-        self.height = value
+        cleans everythings.
+        """
+        sys.stdout = sys.__stdout__
 
-    def __str__(self):
-        """module string represation of square
+    def test_pep8_model(self):
         """
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
-                                                         self.x,
-                                                         self.y,
-                                                         self.width)
+        Tests for pep8 models.
+        """
+        p8 = pep8.StyleGuide(quiet=True)
+        p = p8.check_files(['models/base.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
-    def update(self, *args, **kwargs):
-        """module update square
+    def test_pep8_test(self):
         """
-        if len(args):
-            for q, arg in enumerate(args):
-                if i == 0:
-                    self.id = arg
-                elif q == 1:
-                    self.size = arg
-                elif q == 2:
-                    self.x = arg
-                elif q == 3:
-                    self.y = arg
-        else:
-            for key, value in kwargs.items():
-                if hasattr(self, key) is True:
-                    setattr(self, key, value)
+        Tests for pep8 tests.
+        """
+        p8 = pep8.StyleGuide(quiet=True)
+        p = p8.check_files(['tests/test_models/test_base.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
-    def to_dictionary(self):
-        """retrun dictonary
+    def test_docstrings(self):
+        self.assertIsNotNone(module_doc)
+        self.assertIsNotNone(Base.__doc__)
+        self.assertIs(hasattr(Base, "__init__"), True)
+        self.assertIsNotNone(Base.__init__.__doc__)
+        self.assertIs(hasattr(Base, "create"), True)
+        self.assertIsNotNone(Base.create.__doc__)
+        self.assertIs(hasattr(Base, "to_json_string"), True)
+        self.assertIsNotNone(Base.to_json_string.__doc__)
+        self.assertIs(hasattr(Base, "from_json_string"), True)
+        self.assertIsNotNone(Base.from_json_string.__doc__)
+        self.assertIs(hasattr(Base, "save_to_file"), True)
+        self.assertIsNotNone(Base.save_to_file.__doc__)
+        self.assertIs(hasattr(Base, "load_from_file"), True)
+        self.assertIsNotNone(Base.load_from_file.__doc__)
+
+    def test_id(self):
         """
-        return {
-            "id": self.id,
-            "size": self.size,
-            "x": self.x,
-            "y": self.y
-        }
+        Tests checker for id 
+        """
+        Base._Base__nb_objects = 0
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        b4 = Base(12)
+        b5 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+        self.assertEqual(b4.id, 12)
+        self.assertEqual(b5.id, 4)
+
+    def test_from_json_string(self):
+        """
+        Tests check from json strings.
+        """
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(Base.to_json_string([]), "[]")
+        with self.subTest():
+            r1 = Rectangle(10, 7, 2, 8, 1)
+            r1_dict = r1.to_dictionary()
+            json_dict = Base.to_json_string([r1_dict])
+            self.assertEqual(r1_dict, {'x': 2, 'width': 10,
+                                       'id': 1, 'height': 7,
+                                       'y': 8})
+            self.assertIs(type(r1_dict), dict)
+            self.assertIs(type(json_dict), str)
+            self.assertEqual(json.loads(json_dict), json.loads('[{"x": 2, '
+                                                               '"width": 10, '
+                                                               '"id": 1, '
+                                                               '"height": 7, '
+                                                               '"y": 8}]'))
+
+    def test_rectangle(self):
+        """
+        Tests check for rectangles.
+        """
+        R1 = Rectangle(4, 5, 6)
+        R1_dict = R1.to_dictionary()
+        R2 = Rectangle.create(**R1_dict)
+        self.assertNotEqual(R1, R2)
+
+    def test_square(self):
+        """
+        Tests check for square creations
+        """
+        S1 = Square(44, 55, 66, 77)
+        S1_dict = S1.to_dictionary()
+        S2 = Rectangle.create(**S1_dict)
+        self.assertNotEqual(S1, S2)
+
+    def test_file_rectangle(self):
+        """
+        Test checks if file loads from rectangles.
+        """
+        R1 = Rectangle(33, 34, 35, 26)
+        R2 = Rectangle(202, 2)
+        lR = [R1, R2]
+        Rectangle.save_to_file(lR)
+        lR2 = Rectangle.load_from_file()
+        self.assertNotEqual(lR, lR2)
+
+    def test_file_square(self):
+        """
+        Tests checks if files loads from squared..
+        """
+        S1 = Square(22)
+        S2 = Square(44, 44, 55, 66)
+        lS = [S1, S2]
+        Square.save_to_file(lS)
+        lS2 = Square.load_from_file()
+        self.assertNotEqual(lS, lS2)
