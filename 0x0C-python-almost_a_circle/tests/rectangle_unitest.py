61cde8d7c97efdc122c43bec1e8dd8bb0a209b30
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-07-17 10:30:25 -0700

    Create rectangle_unitest.py

---
+++ 0x0C-python-almost_a_circle/tests/rectangle_unitest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Modules to test the function rectangles"""
+import unittest
+import rectangle
+
+
+class TestRectangle(unittest.TestCase):
+
+    def test_rectangle_output(self):
+        """Functions to test the outputs the functions rectangles"""
+
+        self.assertAlmostEqual(rectangle(,)
+
+    def test_rectangle_input(self):
+        """Functions to test the input the functions rectangles"""
+
+        self.assertRaises(TypeError, rectangle, )
