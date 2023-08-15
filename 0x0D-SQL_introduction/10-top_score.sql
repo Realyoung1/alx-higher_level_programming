60b9f3a961708e7241e1a9bdfb7bcbfa8dc57d93
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-15 16:30:44 -0700

    sdq

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,9 @@
--- Script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Scripts that listed all record of the tables second_table of the databased hbtn_0c_0 in your MySQL server.
+
+
+
+
+
+
+
 SELECT score, name FROM second_table ORDER BY score DESC;
