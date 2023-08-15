290675475c591d29dae54ee8c74afdaffca343f1
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-15 16:31:40 -0700

    sdq

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,9 @@
--- Script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Scripts that listed all recordss with a score >= 10 in the tabless second_table of the databased hbtn_0c_0 in your MySQL server.
+
+
+
+
+
+
+
 SELECT `score`, `name` FROM second_table WHERE `score` >= 10 ORDER BY `score` DESC;
