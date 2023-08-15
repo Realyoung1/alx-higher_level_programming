858303f112106baa02ceda395975f76475a9d72c
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-16 00:43:34 +0100

    sdq

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,9 @@
--- Script that displays the max temperature of each state (ordered by State name)
+-- Scripts that is displaying the maxis temperatured of each stated (ordered by State name)
+
+
+
+
+
+
+
 SELECT state, MAX(value) `max_temp` FROM temperatures GROUP BY state ORDER BY state;
