f7ba87e18fbdbde6a052bd6661168a715f8550eb
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-15 16:05:42 -0700

    sdq

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,3 @@
--- Script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server
+-- Scripts thats lists all rows of the tables first_table from thes databased hbtn_0c_0 in your MySQL server
+
 SELECT * FROM first_table;
