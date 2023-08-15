b1ba92eeeeb3296268d436da5c22ed0802304c62
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-15 16:38:32 -0700

    sdq

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,9 @@
--- Script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Scripts that is listing all records of the tables second_table of the databasesdd hbtn_0c_0 in your MySQL server.
+
+
+
+
+
+
+
 SELECT `score`, `name` FROM second_table WHERE `name` IS NOT NULL AND `name` != "" ORDER BY `score` DESC;
