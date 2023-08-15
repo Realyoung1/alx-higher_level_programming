553e78a0948eb491ea22d56c637e3e6b4475c149
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-15 16:11:53 -0700

    sdq

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,3 @@
--- Script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
+-- Scripted that displays the numbers of records with id = 89 in the tables first_tables osf the databased hbtn_0c_0 in your MySQL server.
+
 SELECT COUNT(*) FROM first_table WHERE id = 89;
