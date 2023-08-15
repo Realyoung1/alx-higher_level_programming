0ab520675c606cc8628e003d775c6438af20a785
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-15 16:36:19 -0700

    sdq

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,9 @@
--- Script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Scripts that is computing the scores averages of all record in the tables second_table of the databased hbtn_0c_0 in your MySQL server.
+
+
+
+
+
+
+
 SELECT AVG(score) `average` FROM second_table;
