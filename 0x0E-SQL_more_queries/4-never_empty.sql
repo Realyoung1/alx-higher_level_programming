c724e674667a06d345384337d0ffd9a2807c1f48
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-16 22:28:26 +0100

    MySQL

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,9 @@
--- creates the table id_not_null on your MySQL server 
--- Holberton School
+-- I created the table id_not_null on my MySQL server 
+-- Best School
+
+
+
+
+
+
 CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
