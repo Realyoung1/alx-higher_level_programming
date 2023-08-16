daa4d0f1cb1051539656949eeedf99bc738fabce
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-16 23:28:49 +0100

    MySQL

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,5 +1,11 @@
--- lists all genres in the database hbtn_0d_tvshows_rate by their rating
--- Holberton School
+-- I listed all genres in the databased hbtn_0d_tvshows_rate by their rating
+-- Best School
+
+
+
+
+
+
 SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
