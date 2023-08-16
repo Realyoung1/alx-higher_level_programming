b9b20bbc2b64c742458e792e2c988b317d932118
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-16 23:19:17 +0100

    MySQL

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,12 @@
--- lists all Comedy shows in the database hbtn_0d_tvshows
--- Holberton School
+-- I lists all Comedy shows in the database hbtn_0d_tvshows
+-- Best School
+
+
+
+
+
+
+
 SELECT tv_shows.title
 FROM tv_shows INNER JOIN tv_show_genres INNER JOIN tv_genres 
 ON tv_genres.name = "Comedy" AND tv_show_genres.genre_id = tv_genres.id
