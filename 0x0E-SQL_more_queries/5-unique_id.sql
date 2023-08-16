4e66f326e415aeecc623081b9a4818323e6c4611
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-16 22:30:30 +0100

    MySQL

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,9 @@
--- creates the table unique_id on your MySQL server 
--- Holberton School
+-- created the table unique_id on my MySQL server 
+-- Best School
+
+
+
+
+
+
 CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
