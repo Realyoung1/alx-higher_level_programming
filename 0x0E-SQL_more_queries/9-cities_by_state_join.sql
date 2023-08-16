aaeabf274f1d2446a071064485e08e66656d9107
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-16 23:05:50 +0100

    MySQL

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,9 @@
--- lists all cities contained in the database hbtn_0d_usa
--- Holberton School
+-- I listed all cities contained in the database hbtn_0d_usa
+-- Best School
+
+
+
+
+
+
 SELECT c.id, c.name, s.name FROM cities AS c, states AS s WHERE c.state_id = s.id;
