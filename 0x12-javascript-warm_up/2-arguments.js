38eace8c2887654183c4eea98253ac5781cc27c6
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-11 15:19:47 -0700

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 /*
   writing a script that pass a message depending on the number of args
 */
+
 const args = process.argv;
-if (args.length <= 2) {
-  console.log('No argument');
-} else if (args.length === 3) {
+if (args.length <= 2) {console.log('No argument');}
+else if (args.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
