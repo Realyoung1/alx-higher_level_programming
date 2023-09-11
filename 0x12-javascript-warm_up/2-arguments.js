072c3797676b0286cb33effac3913e125daac5cc
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-11 15:10:01 -0700

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,5 +2,6 @@
 /*
   print Found or not if exists input arguments
 */
+#!/usr/bin/node
 const count = process.argv.length;
 console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
