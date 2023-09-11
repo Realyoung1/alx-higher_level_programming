0e26f6225a5a3d0707417f131996e7b257257e31
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-11 15:32:03 -0700

    Create 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+ this has printed some line about 3 in loops
+*/
+
+
+const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const line of args) {
+  console.log(line);
+}
