98fa7319d5dd24fe2105e16274299ab9c34b6873
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-11 14:33:45 -0700

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 /*
-  func for the script printing three lines
+  func for the scripts printings three lines
 */
-const myVar = C is fun;
-                Python is cool;
-                      Javascript is amazing;
+const myVar = 'C is fun\n' + 'Python is cool\n' + 'Javascript is amazing';
+
 console.log(myVar);
