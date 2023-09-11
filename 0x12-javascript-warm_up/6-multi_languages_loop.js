8a5bfe10720b1b7cdee3a701ad7a5c6ffbd5e8d0
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-11 16:20:39 -0700

    warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,8 +3,6 @@
  this has printed some line about 3 in loops
 */
 
-const args('C is fun\nPython is cool\nJavaScript is amazing');
-
-for (const line of args) {
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(line);
 }
