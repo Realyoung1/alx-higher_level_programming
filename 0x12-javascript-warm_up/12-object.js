0fcd91a6efaeaebfe5293542bf2576d1869810be
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-11 15:50:47 -0700

    Create 12-object.js

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+  script that updated the value of 12 and 89
+*/
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
