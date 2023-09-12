72eff4f44b96f5e0b27d340af68b6f367392880d
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-12 06:58:47 -0700

    objects

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
 /*
-     converts a number from base 10 to another base passed as argument
+     funcs which convert a numb from base 10 to another base passed as args
+     Prototype: exports.converter = function (base)
+     You are not allowed to import any file
+     You are not allowed to declare any new variable (var, let, etc..)
+
 */
+
 exports.converter = function (base) {
   return function (number) {
     return number.toString(base);
