153a1baaf9bf9e247dc54759b1c4cbd56afc8f5e
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-12 06:35:15 -0700

    objects

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 /*
-    Class 2 init arg if are int and > 0
-    create a print method
+    rectangle classes that defines rectangle int and > 0
+    createding of a printing methods
 */
-
 class Rectangle {
   constructor (w, h) {
     if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
@@ -11,12 +10,10 @@ class Rectangle {
       this.height = h;
     }
   }
-
   print () {
     for (let height = 0; height < this.height; height++) {
       console.log('X'.repeat(this.width));
     }
   }
 }
-
 module.exports = Rectangle;
