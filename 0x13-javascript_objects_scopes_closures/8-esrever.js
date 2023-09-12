cc0a1c45695ac61c92d8c5d984d0b1b28a95748f
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-12 06:53:58 -0700

    objects

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 /*
-    returns the reversed version of a list
+    this is the func which returned the reverse version of list
+    Prototype: exports.esrever = function (list)
+    not allowed to use the built in method
 */
-
 exports.esrever = function (list) {
   const revList = [];
   for (let index = list.length - 1; index >= 0; index--) {
