a86978a073d93bad80b05f53f456e133eed947e2
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-12 07:00:58 -0700

    objects

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 /*
-     imports an array and computes a new array
+     func that imported the array and computed new array
+     Your script must import list from the file 100-data.js
+     Print both the initial list and the new list
 */
-const list = require('./100-data').list;
 
+const list = require('./100-data').list;
 const newList = list.map((element, index) => element * index);
 console.log(list);
 console.log(newList);
