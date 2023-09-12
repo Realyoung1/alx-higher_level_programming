f9f13dd23ca2e4f69edcfc490b362f52fd961705
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-12 06:54:45 -0700

    Delete 0x13-javascript_objects_scopes_closures/9-logme(1).js

--- 0x13-javascript_objects_scopes_closures/9-logme(1).js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-/*
-     prints the number of arguments already printed and the new argument value
-*/
-let count = 0;
-exports.logMe = function (item) {
-  console.log(`${count}: ${item}`);
-  count += 1;
-};
