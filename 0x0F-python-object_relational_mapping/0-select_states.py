724cef4f1bf708fc3d39bbf782b9544eb5264c20
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-14 10:39:19 -0700

    SQL

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 """
-    Get all states of a data base
+    the scripts listed all states from the database hbtn_0e_0_usa
+    this scripts takes two args
+    this script conncected with MySQL
+    I sorted the result in an ascending order
+    The result was displayed based on the given examples
+    I make sure my code was not executed when imported
 """
 import MySQLdb
 from sys import argv
 
-
 def main():
-    """Only executes when is not imported"""
+    """I only executed my codes is not imported"""
     db = MySQLdb.connect(host="localhost",
                          user=argv[1],
                          port=3306,
                          passwd=argv[2],
                          db=argv[3])
     c = db.cursor()
-    numrows = c.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+    numrows = c.execute("""SELECT * FROM states""")
     states = c.fetchall()
     for idstate in states:
         print(idstate)
