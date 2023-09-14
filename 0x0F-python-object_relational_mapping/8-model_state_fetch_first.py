17a017d0f8b5f06a6346b1a37c38f34f40da9e39
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-14 11:00:33 -0700

    SQL

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """
-    Get the states of database
+    This scrpted printed the first state objects
+    this scripts takes two args
+    this script conncected with MySQL
+    I sorted the result in an ascending order
+    The result was displayed based on the given examples
+    I make sure my code was not executed when imported
 """
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
