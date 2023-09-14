76d915afe493fe2fc905859c386f50fac75bdbcc
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-14 11:32:42 -0700

    SQL

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 """
-    Add a state and a city of database
+    improving the cities
+    states one databases
+    this scripts takes two args
+    this script conncected with MySQL
+    I sorted the result in an ascending order
+    The result was displayed based on the given examples
+    I make sure my code was not executed when imported
 """
 import sys
 from relationship_state import State
 from relationship_city import City, Base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
