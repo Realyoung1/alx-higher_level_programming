919bcdbab3fee6864191a5c11a348aebeab2fdc2
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-14 11:24:42 -0700

    SQL

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """
-    Add the states of database
+    Adding the states of Louisina
+    this scripts takes two args
+    this script conncected with MySQL
+    I sorted the result in an ascending order
+    The result was displayed based on the given examples
+    I make sure my code was not executed when imported
 """
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
