38c1893ba287f6945767f0105721806509eff9dd
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-28 15:52:11 -0700

    netwrok

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script bahs url and send post, a variable email, subject and uses curl
-curl -s -X POST -L "$1" -d 'email=test@gmail.om&subject=I will always be here for PLD'
+curl -s -X POST -L "$1" -d 'email=test@gmail.com&subject=I will always be here for PLD'
