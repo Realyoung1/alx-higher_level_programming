2a0905219d9c735e302aa8ec2fca6fa111b82732
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-29 10:40:04 -0700

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,13 +7,14 @@
      i used the "with" statement
 """
 
-from urllib import (request)
 
+from urllib import (request)
 if __name__ == "__main__":
     req = request.Request("https://alx-intranet.hbtn.io/status")
     with request.urlopen(req) as response:
         body = response.read()
 
+     
     print("Body response:")
     print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body))
