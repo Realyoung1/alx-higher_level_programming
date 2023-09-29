c92351ee730395e8e7e96fd66a7ecf548cb9e5fe
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-29 10:41:00 -0700

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     req = request.Request("https://alx-intranet.hbtn.io/status")
     with request.urlopen(req) as response:
         body = response.read()
-
      
     print("Body response:")
     print("\t- type: {}".format(type(body)))
