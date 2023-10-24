89a4137ee3e665732a59321e553707978084974a
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-10-24 14:28:29 -0700

    js web s

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
 /*
-    gets the contents of a webpage and stores it in a file
+    scripted got the content of a webpage nd stores it in a file
+    when the first arg is the URL request
+    secodn arg is the file stored
+    file was UTF-8 encoded
+    module request used
 */
+
 const request = require('request');
 const url = process.argv[2];
 const fileName = process.argv[3];
