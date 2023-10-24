41781e272ef5fe1dbac1e4b3a462ac146cbe1ab6
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-10-24 14:18:54 -0700

    js web s

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 /*
-     display the status code of a GET request
+     it displaed  the status code GET request
+     when the first args is the url GET
+     we have status code printed in code: <status code>
+     module equest was used
 */
+
 const request = require('request');
 const url = process.argv[2];
 
