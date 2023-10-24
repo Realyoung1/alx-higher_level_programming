02cbf4ac4b9ebb6791a07ccafc9c7a4885bbb9e2
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-10-24 14:16:36 -0700

    js web s

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 /*
-     writes a string to a file
+     when first args is the first path
+     when the second args was the string
+     i make sure the content of the file read utf-8
+     th error object was print in case an erro occur
 */
+
 const fs = require('fs');
 const nameFile = process.argv[2];
 const str = process.argv[3];
